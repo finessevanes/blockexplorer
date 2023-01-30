c6e9c13110a268de7b1ed029e3145b7fe6b248d6
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2023-01-29 22:04:54 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Alchemy, Network } from "alchemy-sdk";
+import { Alchemy, Network, Utils } from "alchemy-sdk";
 import { useEffect, useState } from "react";
 
 import "./App.css";
@@ -21,20 +21,16 @@ const alchemy = new Alchemy(settings);
 function App() {
   const [blockNumber, setBlockNumber] = useState();
   const [searchValue, setSearchValue] = useState("");
+  const [blockTransactions, setBlockTransactions] = useState([]);
+  const [blockData, setBlockData] = useState();
+  const entriesToShow = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // make a call to the API here using the searchValue
-    // to retrieve data for the specific block number
-    // axios
-    //   .get(`https://api.etherscan.io/api?module=block&action=getblockreward&blockno=${searchValue}`)
-    //   .then(response => {
-    //     setBlockData(response.data);
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
+    console.log(searchValue);
   };
+
+  console.log(blockTransactions)
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -44,39 +40,58 @@ function App() {
     getBlockNumber();
   });
 
+  useEffect(() => {
+    async function getBlockData() {
+      await alchemy.core
+        .getBlockWithTransactions(blockNumber)
+        .then((response) => {
+          setBlockData(response);
+          setBlockTransactions(response.transactions);
+        });
+    }
+    getBlockData();
+  }, [blockNumber]);
+
   return (
-    <>
-      <div className="App">Block Number: {blockNumber}</div>
-      <div className="bg-gray-900 text-white min-h-screen p-8">
-        <h1 className="text-3xl font-medium mb-4">Block Explorer</h1>
-        <form className="mb-4 max-w-max flex" onSubmit={handleSubmit}>
-          <label className="block text-sm font-medium mb-2" htmlFor="search">
-            Search for a block:
-          </label>
-          <input
-            className="bg-gray-800 rounded-lg p-2 text-white w-full"
-            type="text"
-            id="search"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            placeholder="Enter a block number"
-          />
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
-            Search
-          </button>
-        </form>
-        {1 === 1 && (
-          <div className="bg-gray-800 rounded-lg p-4">
+    <div className="bg-gray-900 text-white min-h-screen p-8">
+      <h1 className="text-3xl font-medium mb-4">Block Explorer</h1>
+      <form className="mb-4 max-w-max flex" onSubmit={handleSubmit}>
+        <label className="block text-sm font-medium mb-2" htmlFor="search">
+          Search for a block:
+        </label>
+        <input
+          className="bg-gray-800 rounded-lg p-2 text-white w-full"
+          type="text"
+          id="search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Enter a block number"
+        />
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
+          Search
+        </button>
+      </form>
+      <div className="flex">
+        <div>
+          <h1 className="text-lg font-medium mb-2">Latest Blocks</h1>
+          {entriesToShow.map((num, i) => (
+            <div key={i} className="bg-gray-800 rounded-lg p-4 basis-1/2 mb-2">
+              <h2 className="text-lg font-medium mb-2">
+                Block Number: {blockNumber - num}
+              </h2>
+            </div>
+          ))}
+        </div>
+        <div className="ml-20">
+          <h1 className="text-lg font-medium mb-2">Latest Transactions</h1>
+          <div className="bg-gray-800 rounded-lg p-4 basis-1/2">
             <h2 className="text-lg font-medium mb-2">
-              Block Number: {blockNumber}
+              Block Tran: {blockNumber}
             </h2>
-            <h2 className="text-lg font-medium mb-2">Block Reward: </h2>
-            <h2 className="text-lg font-medium mb-2">Gas Used: </h2>
-            <h2 className="text-lg font-medium mb-2">Gas Limit: </h2>
           </div>
-        )}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
