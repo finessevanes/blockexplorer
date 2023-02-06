6be9c233c66ed12cc355d681e21e030f4eb8dd5f
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2023-02-05 18:02:46 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -97,8 +97,8 @@ function App() {
           </div>
           <div className="ml-20">
             <h1 className="text-lg font-medium mb-2">Latest Transactions</h1>
-            {blockTransactions.map((transaction, i) => (
-              <div className="bg-gray-800 rounded-lg p-4 basis-1/2 mb-2">
+            {blockTransactions.slice(0,10).map((transaction, i) => (
+              <div className="bg-gray-800 rounded-lg p-4 basis-1/2 mb-2" key={i}>
                 <h2 className="text-lg font-medium mb-2">
                   Block Transactions:{" "}
                   {`${transaction.hash.slice(0, 5)}...${transaction.hash.slice(
